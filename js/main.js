949dfe986fe7dee38a3d8ecf479b0a674ab0046a
guidojuliano <guidojulianou@gmail.com>
2022-05-08 20:43:15 -0300

    responsive detalles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 addEventListener("DOMContentLoaded", () => {
+  const btn_menu = document.querySelector(".btn-menu");
+  if (btn_menu) {
+    btn_menu.addEventListener("click", () => {
+      const menu_items = document.querySelector(".menu_items");
+      menu_items.classList.toggle("show");
+    });
+  }
+
   const sections = document.querySelectorAll(".section");
   const menuItems = document.querySelectorAll(".menu_item");
 
